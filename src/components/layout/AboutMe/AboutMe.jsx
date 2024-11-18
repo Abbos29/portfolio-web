0cf7bcb8bd567f541f450155598ef0c6e8a56151
Abbos29 <abbosvaliev2003@gmail.com>
2024-11-18 13:34:23 +0500

    feat: finish v1

--- src/components/layout/AboutMe/AboutMe.jsx
+++ src/components/layout/AboutMe/AboutMe.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import s from './AboutMe.module.scss'
+import Top from '../../ui/Top/Top'
+import { FcIdea } from 'react-icons/fc'
+
+
 
 const AboutMe = () => {
     return (
         <>
             <section className={s.aboutme}>
-                <div className="container">
-                    <h1>About me</h1>
+                <div className={s.about_wrap}>
+                    <div className={s.box}>
+                        <Top title={'About'} />
+                        <p>Hey, my name is Charles Bruyerre and I use Sharlee as my nickname across social medias. I’m a graphic designer, UX/UI designer & front-end web developer from France. I’m also passionate about pop music and make portraits and universes around what I listen to and I’m always curious to learn more when it comes to new technologies and creative coding.</p>
+                        <a href="">↓ Resume</a>
+                    </div>
+
+                    <div className={s.image}>
+                        <div className={s.idea}>
+                            <div>
+                                <FcIdea />
+
+                            </div>
+                            {/* <p>abbos valiev • abbos valiev • abbos valiev •</p> */}
+        
+                        </div>
+                        <img src="/profile.avif" alt="image" />
+                    </div>
                 </div>
             </section>
         </>
--- src/components/layout/Contact/Contact.jsx
+++ src/components/layout/Contact/Contact.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import s from './Contact.module.scss'
+import Top from '../../ui/Top/Top'
 
 const Contact = () => {
     return (
         <>
             <section className={s.contact}>
                 <div className="container">
-                    <h1>Contacts</h1>
+                    <div className={s.wrap}>
+                        <Top title={'Contacts'} />
+                        <div className={s.inner}>
+                            <div className={s.item}>
+                                <h3>MAIL</h3>
+                                <div>
+                                    <a href="mailto:abbosvaliev2003@gmail.com">↗ abbosvaliev2003@gmail.com</a>
+                                </div>
+                            </div>
+
+                            <div className={s.item}>
+                                <h3>Social medias</h3>
+                                <div>
+                                    <a href="">↗ telegram</a>
+                                    <a href="">↗ whatsapp</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
